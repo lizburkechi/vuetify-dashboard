77eb8af37f5104ec8fd49f3959c23d0cdec40c50
Liz Burke <lizburke@MacBook-Pro-2.attlocal.net>
2021-12-27 18:03:46 -0600

    vue router

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,31 @@
 		<v-app-bar app color="primary" dark>
 			<v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
 			<v-spacer></v-spacer>
-			<v-btn text rounded>Home</v-btn>
-			<v-btn text rounded>Login</v-btn>
+			<v-btn
+			v-for="link in links"
+			:key="`${link.label}-header-link`"
+			text 
+			rounded 
+			:to="link.url"
+			>
+			{{link.label}}
+			</v-btn>
 		</v-app-bar>
 		<v-content>
-	
+			<router-view></router-view>
 		</v-content>
 		<v-footer color="primary lighten-1" padless>
 			<v-layout justify-center wrap>
 				<v-btn
 					v-for="link in links"
-					:key="link"
+					:key="`${link.label}-footer-link`"
 					color="white"
 					text
 					rounded
 					class="my-2"
+					:to="link.url"
 				>
-					{{ link }}
+					{{ link.label }}
 				</v-btn>
 				<v-flex primary lighten-2 py-4 text-center white--text xs12>
 					{{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
@@ -35,8 +43,18 @@ export default {
 	data() {
 		return {
 			links: [
-				'Home', 
-				'Login'
+				{
+					label: 'Home',
+					url: '/'
+				},
+				{
+					label: 'Login',
+					url: '/login'
+				},
+				{
+					label: 'Dashboard',
+					url: '/dashboard'
+				}
 			]
 		}
 	}
